beb13d4e5ab1e31e5d8e1e32bf4f5bc3d6de8374
Supreet <supreetkumar32@gmail.com>
2026-01-20 21:54:28 +0530

    maximum sum subarray fix

--- dsaPrac2026/src/arrays/MaxSumSubarray.java
+++ dsaPrac2026/src/arrays/MaxSumSubarray.java
@@ -4,20 +4,26 @@ public class MaxSumSubarray {
 
 	public static void main(String[] args) {
 		
-		int a[]= {-11,-2,-3,-4,-2,-5,-6};
-		int max=Integer.MIN_VALUE;
-		int sum=0;
-		int start=0,ansStart=-1,ansEnd=-1;//for tracking
-		for(int i=0;i<a.length;i++) {
-			if(sum==0) start=i;//for tracking
-			sum=sum+a[i];
-			if(sum>max) {
-				max=sum;
-				ansStart=start;ansEnd=i;//for tracking
-			}
-			if(sum<0) {
-				sum=0;
-			}
+		int a[] = {-11, -2, -3, -4, -2, -5, -6};
+
+		int max = a[0];
+		int sum = a[0];
+
+		int start = 0, ansStart = 0, ansEnd = 0;
+
+		for (int i = 1; i < a.length; i++) {
+		    if (sum + a[i] < a[i]) {
+		        sum = a[i];
+		        start = i;
+		    } else {
+		        sum += a[i];
+		    }
+
+		    if (sum > max) {
+		        max = sum;
+		        ansStart = start;
+		        ansEnd = i;
+		    }
 		}
         System.out.println(max);
         System.out.println(ansStart);
